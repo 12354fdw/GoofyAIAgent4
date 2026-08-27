e6e00f2a318bb2d8987769eed2992e22ae10c3e4
12354fdw <88530369+12354fdw@users.noreply.github.com>
2026-08-27 13:37:24 +0700

    refactor: made the import at the top in ENTRY_CLIENT

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,10 +1,12 @@
 import { readFileSync } from "node:fs";
+import { ComlinkClient } from "./networking/comlinkClient.js";
 
 const client = new ComlinkClient();
 
 async function runAgent(prompt: string) {
+	let text = "";
 	for await (const part of client.stream("default-session", prompt)) {
-		if (part.type === "finished") return;
+		if (part.type === "finished") break;
 
 		if (part.type === "step_end") {
 			process.stdout.write("\n-[STEP END]-\n");
@@ -13,11 +15,13 @@ async function runAgent(prompt: string) {
 
 		if (part.type !== "token") continue;
 		process.stdout.write(part.content);
+		text += part.content;
 	}
-	process.stdout.write("\n----------------------------\n");
-}
 
-import { ComlinkClient } from "./networking/comlinkClient.js";
+	process.stdout.write("\n----------------------------\n");
+
+	return text;
+}
 
 export async function ENTRY_CLIENT() {
 	runAgent(readFileSync("prompt.txt", "utf-8"));
