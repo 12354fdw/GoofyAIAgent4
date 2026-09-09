21bbbcae80240c10f97aec4d9d580190b9dc8b07
12354fdw <88530369+12354fdw@users.noreply.github.com>
2026-09-09 12:46:12 +0700

    feat: hook up ClientSession into App

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,29 +1,18 @@
-import WebSocket from "ws";
 import { ComlinkClient } from "./networking/comlinkClient.js";
 import { ComlinkClientNetworking } from "./networking/comlinkClientNetworking.js";
-import { CheckpointDeltaDecoder } from "./networking/checkpointDeltaDecoder.js";
 import { ClientSession } from "./clientSession.js";
 
 export class Client {
 	private rpc: ComlinkClient;
-	private decoder: CheckpointDeltaDecoder;
 
-	private constructor(
-		private networking: ComlinkClientNetworking,
-		private streamSocket: WebSocket,
-	) {
+	private constructor(private networking: ComlinkClientNetworking) {
 		this.rpc = new ComlinkClient(networking);
-
-		this.decoder = new CheckpointDeltaDecoder(streamSocket);
 	}
 
 	public static async create() {
 		const networking = new ComlinkClientNetworking(4613);
 		await networking.awaitReady();
-
-		const streamSocket = await networking.createCheckpointSocket();
-
-		const instance = new Client(networking, streamSocket);
+		const instance = new Client(networking);
 		return instance;
 	}
 
