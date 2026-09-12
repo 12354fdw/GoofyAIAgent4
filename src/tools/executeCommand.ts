a3c2d73dc64209456173ea0c757cac4e3681a69d
acoolmanboon <acoolmanboon@gmail.com>
2026-09-12 19:30:03 +0700

    improve executeCommand
    - use promisify instead of lambda function or something
    - return exit code instead of arbitrary 124

--- src/tools/executeCommand.ts
+++ src/tools/executeCommand.ts
@@ -1,8 +1,12 @@
 import { tool } from "ai";
 import { exec } from "node:child_process";
+import { promisify } from "node:util";
 import z from "zod";
 import { LOGGER } from "../shared/globals/logger.js";
 import os from "node:os";
+
+const execAsync = promisify(exec);
+const MAX_BUFFER = 1 * 1024 * 1024
 
 function getOsAgentDescription(platformName: string): string {
 	switch (platformName) {
@@ -35,28 +39,33 @@ export const Tool_ExecuteCommand = tool({
 		getOsAgentDescription(os.platform()),
 	inputSchema: z.object({
 		cmd: z.string().describe("The command CANNOT be any interactive commands, including sudo."),
-		timeout: z.number().default(10).describe("timeout, defaults to 10"),
+		timeout: z.number().int().min(1).max(60).default(10).describe("Timeout in seconds."),
 	}),
 
 	needsApproval: true,
 	execute: async ({ cmd, timeout }) => {
-		return new Promise<{ stdout: string; stderr: string; exitCode: number }>((resolve, reject) => {
-			LOGGER.warn(`executing command '${cmd}' with timeout ${timeout}s`);
-			exec(cmd, { timeout: timeout * 1000 }, (error, stdout, stderr) => {
-				if (error && error.killed) {
-					reject({
-						stdout,
-						stderr: stderr || `Command timed out after ${timeout}s`,
-						exitCode: 124,
-					});
-					return;
-				}
-				resolve({
-					stdout,
-					stderr,
-					exitCode: error ? (typeof error.code === "number" ? error.code : 1) : 0,
-				});
-			});
-		});
+		LOGGER.warn(`Executing command '${cmd}' with timeout ${timeout}s`);
+
+		try {
+			const { stdout, stderr } = await execAsync(cmd, { timeout: timeout * 1000 + 1000, windowsHide: true, maxBuffer: MAX_BUFFER });
+
+			return {
+				stdout: stdout,
+				stderr,
+				exitCode: 0,
+			};
+		} catch (error) {
+			const err = error as NodeJS.ErrnoException & {
+				stdout?: string;
+				stderr?: string;
+				signal?: string;
+			};
+
+			return {
+				stdout: err.stdout ?? "",
+				stderr: err.stderr ?? err.message,
+				exitCode: typeof err.code === "number" ? err.code : 1,
+			};
+		}
 	},
 });
