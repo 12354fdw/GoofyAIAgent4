805a30df2645484b616072818809764a25215440
12354fdw <88530369+12354fdw@users.noreply.github.com>
2026-09-13 14:18:35 +0700

    feat: implemented Command

--- src/client/commands/command.ts
+++ src/client/commands/command.ts
@@ -1,16 +1,23 @@
 import z from "zod";
+import { CommandContext } from "./commandContext.js";
 
-export class Command {
-	constructor(name: string) {
+type InferSchemaShape<T extends Record<string, z.ZodTypeAny>> = {
+	[K in keyof T]: z.infer<T[K]>;
+};
 
+export class Command<TParams extends Record<string, z.ZodTypeAny> = Record<string, z.ZodTypeAny>> {
+	private parameters: Record<string, z.ZodTypeAny> = {};
+	private executor?: (ctx: CommandContext, args: InferSchemaShape<TParams>) => Promise<void> | void;
+
+	constructor(public readonly name: string) {}
+
+	public parameter<K extends string, T extends z.ZodTypeAny>(name: K, type: T): Command<TParams & { [P in K]: T }> {
+		this.parameters[name] = type;
+		return this as unknown as Command<TParams & { [P in K]: T }>;
 	}
 
-	public parameter(name: string, type: z.ZodAny) {
-
-	}
-
-	// TODO: give ctx an actual type
-	public execute(cb: (ctx: unknown, args: object) => Promise<void>) {
-
+	public execute(cb: (ctx: CommandContext, args: InferSchemaShape<TParams>) => Promise<void> | void) {
+		this.executor = cb;
+		return this;
 	}
 }
---
+++ src/client/commands/commandContext.ts
@@ -0,0 +1,3 @@
+export interface CommandContext {
+	sendMessage: (message: string) => void;
+}
