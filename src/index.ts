93db6bdfe44b2d9bedf858fa910de1e98ed59e1a
12354fdw <88530369+12354fdw@users.noreply.github.com>
2026-09-17 21:14:08 +0700

    feat: made it automatically create DATA_DIRECTORY if it doesn't exist

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import fs from "fs";
 import { ENTRY_SERVER } from "./server/index.js";
 import { ENTRY_CLIENT } from "./cli/index.js";
 import { DATA_DIRECTORY } from "./shared/globals/index.js";
+import { SessionStore } from "./server/storage/SessionStore.js";
 
 fs.mkdirSync(DATA_DIRECTORY, { recursive: true });
+SessionStore.getInstance();
 
 ENTRY_SERVER();
 ENTRY_CLIENT();
