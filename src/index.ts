90aef1fe27d397e52615a914b1db34299987f747
12354fdw <88530369+12354fdw@users.noreply.github.com>
2026-09-18 09:48:48 +0700

    perf: made the schema more efficient

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,9 @@ import { ENTRY_SERVER } from "./server/index.js";
 import { ENTRY_CLIENT } from "./cli/index.js";
 import { DATA_DIRECTORY } from "./shared/globals/index.js";
 import { SessionStore } from "./server/storage/SessionStore.js";
+import path from "path";
 
-fs.mkdirSync(DATA_DIRECTORY, { recursive: true });
+fs.mkdirSync(path.join(DATA_DIRECTORY, "db"), { recursive: true });
 SessionStore.getInstance();
 
 ENTRY_SERVER();
--- src/server/storage/SessionStore.ts
+++ src/server/storage/SessionStore.ts
@@ -2,24 +2,43 @@ import Database from "better-sqlite3";
 import path from "path";
 import { DATA_DIRECTORY } from "../../shared/globals/index.js";
 
-export class SessionStore {
-	private static instance: SessionStore;
-	private sessionsDb = new Database(path.join(DATA_DIRECTORY, "sessions.sqlite3"));
+export const DATABASE_SCHEMA_VERSION = 1;
 
-	private constructor() {
-		this.sessionsDb.exec(`
+function getDbFromName(dbName: string) {
+	const db = new Database(path.join(DATA_DIRECTORY, "db", `${dbName}.sqlite3`));
+	db.exec(`
 			PRAGMA journal_mode = WAL;
 			PRAGMA synchronous = NORMAL;
 			PRAGMA foreign_keys = ON;
-			PRAGMA user_version = 1;
+	`);
+
+	const version = db.pragma("user_version", { simple: true }) as number;
+	if (version !== 0 && version !== DATABASE_SCHEMA_VERSION) {
+		throw new Error("data migration not implemented yet!");
+	}
+
+	db.exec(`PRAGMA user_version = ${DATABASE_SCHEMA_VERSION}`);
+
+	return db;
+}
+
+export class SessionStore {
+	private static instance: SessionStore;
+	private readonly sessionsDataDb = getDbFromName("sessionsData");
+	private readonly sessionsCheckpointDb = getDbFromName("sessionsCheckpoints");
+	private readonly sessionsMessagesDb = getDbFromName("sessionsModelMessages");
+
+	private constructor() {
+		this.sessionsDataDb.exec(`
+			PRAGMA journal_mode = WAL;
+			PRAGMA synchronous = NORMAL;
+			PRAGMA foreign_keys = ON;
+			PRAGMA user_version = ${DATABASE_SCHEMA_VERSION};
 		`);
 
-		this.sessionsDb.exec(`CREATE TABLE IF NOT EXISTS sessions (
+		this.sessionsDataDb.exec(`CREATE TABLE IF NOT EXISTS sessions (
 			session_name	TEXT PRIMARY KEY,
-
-			usage 			TEXT,
-			checkpoints 	TEXT,
-			model_messages	TEXT
+			usage 			TEXT
 		)`);
 	}
 
